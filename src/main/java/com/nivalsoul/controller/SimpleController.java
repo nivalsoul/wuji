3d26531a8a51e1212ec4edb2bd1cdc5f3478659d
nivalsoul <kskscr@163.com>
2015-11-22 20:23:19 +0800

    修改为支持tomcat7和jdk7

--- src/main/java/com/nivalsoul/controller/SimpleController.java
+++ src/main/java/com/nivalsoul/controller/SimpleController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
